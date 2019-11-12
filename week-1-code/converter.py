97da949e18bb36f3e34f78b1574d4bd70bff6cb5
Jeff Scott <jeffscott2@gmail.com>
2019-11-11 23:09:37 -0500

    undo a delete

---
+++ week-1-code/converter.py
@@ -0,0 +1,11 @@
+
+meter_input = float(input("Enter A Distance In Meters: "))
+
+converter = 3.28084
+
+feet = meter_input * converter
+
+result_str = "That Is Equal To " + str(feet) +" Feet"
+print(result_str)
+
+
