f7aa5f429b8f62518f8da980323ba3e1a5eda4f4
Jeff Scott <jeffscott2@gmail.com>
2019-11-12 14:39:09 -0500

    Rename map -> dictionary

---
+++ week-2-code/inventory-dictionary.py
@@ -0,0 +1,45 @@
+
+class Inventory:
+    # 0 <= building_idx < building_count
+    def __init__(self):
+        
+        self.buildings = {}
+        self.log = []
+        self.day = 1
+    
+    def add_inventory(self, building_key, count):
+        self.buildings[building_key] = self._get_current_inventory_level(building_key) + count
+        self.add_log_entry(f"Building {building_key} added {count}")
+
+    def remove_inventory(self, building_key, count):
+        self.buildings[building_key] = self._get_current_inventory_level(building_key) - count
+        self.add_log_entry(f"Building {building_key} removed {count}")
+
+    def _get_current_inventory_level(self, building_key):
+        default_count = 0
+        return self.buildings.get(building_key, default_count)
+
+    def print_inventory_by_building(self):
+        for key in self.buildings.keys():
+            print(f"Building {key}: {self.buildings[key]}")
+    
+    def print_inventory_total(self):
+        total_inventory = sum(self.buildings.values())
+        print(f"Total Inventory: {total_inventory}")
+
+    def add_log_entry(self, message):
+        self.log.append(message)
+
+
+
+i = Inventory()
+
+i.add_inventory(1,3)
+i.add_inventory(1,3)
+i.add_inventory(2,1)
+i.remove_inventory(1,1)
+i.add_inventory('the garage',1)
+i.add_inventory('the shed',7)
+i.print_inventory_by_building()
+i.print_inventory_total()
+# i.print_log()
