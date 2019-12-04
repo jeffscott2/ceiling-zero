a8be99a672f7cce4372cceca8f7c3c4c4e355a5d
Jeff Scott <jeffscott2@gmail.com>
2019-12-04 17:45:03 -0500

    tweaks

--- week_5_code/etl/master.sh
+++ week_5_code/etl/master.sh
@@ -13,16 +13,14 @@ db_host="127.0.0.1"
 db_user="root"
 db_schema="housing"
 
-
 echo "# Starting ETL"
 
 # Extract the data from a url (APIs fix here nicely too)
 source_url="https://raw.githubusercontent.com/jeffscott2/ceiling-zero/master/week_5_code/kc_house_data.csv"
 echo "E: Extracting data from URL to ${price_data_file_raw}"
 echo "$ curl -s $source_url -o $price_data_file_raw"
-echo ""
 #curl -s $source_url -o $price_data_file_raw
-
+echo ""
 
 
 # Transform by fixing the exponential notation
@@ -31,11 +29,10 @@ echo "$ cat $price_data_file_raw \
 | sed 's/\(.\)\./\1/' \
 | sed 's/E\+06/0000/' > $price_data_file_clean
 "
-echo ""
 #cat $price_data_file_raw \
 #| sed 's/\(.\)\./\1/' \
 #| sed 's/E\+06/0000/' > $price_data_file_clean
-
+echo ""
 
 # Load
 echo "L: Generate SQL to insert into the DB"
@@ -43,28 +40,25 @@ echo "$ cat $price_data_file_clean \
 | grep -v "price" \
 | awk '{print \"insert into kc_prices(price,bedrooms,bathrooms,sqft_living,sqft_lot,floors,waterfront,view,cond,grade,yr_built,yr_renovated,zipcode) select \" \$0 \";\"}' \
 > $price_data_file_sql"
-echo ""
-
 #cat $price_data_file_clean \
 #| grep -v "price" \
 #| awk '{print "insert into kc_prices(price,bedrooms,bathrooms,sqft_living,sqft_lot,floors,waterfront,view,cond,grade,yr_built,yr_renovated,zipcode) select " $0 ";"}' \
 #> $price_data_file_sql
-
+echo ""
 
 echo "L: Clean the DB table"
 echo "$ delete_sql=\"delete from kc_prices\""
 echo "$ mysql -e \"\$delete_sql\" -h $db_host -u $db_user $db_schema"
-echo ""
 #delete_sql="delete from kc_prices"
+echo ""
 #mysql -e "$delete_sql" -h $db_host -u $db_user $db_schema
 
 
 
 echo "L: Loading clean data to mysql"
 echo "$ mysql -h $db_host -u $db_user $db_schema < $price_data_file_sql"
-echo ""
 #mysql -h $db_host -u $db_user $db_schema < $price_data_file_sql
-
+echo ""
 
 
 
@@ -77,17 +71,15 @@ echo "# Starting Analysis"
 
 echo "Exporting CSV of large houses for processing to $price_data_analysis_input"
 echo " Filtering houses to those with #bedrooms >= $min_num_rooms_for_analysis"
-echo "$ python3 db_data_query.py 2 > $price_data_analysis_input"
+echo "$ python3 db_data_query.py $min_num_rooms_for_analysis > $price_data_analysis_input"
+# python3 db_data_query.py $min_num_rooms_for_analysis > $price_data_analysis_input
 echo ""
-# python3 db_data_query.py 2 > $price_data_analysis_input
-
 
 
 echo "Running Analysis with R"
 echo "$ Rscript bootcamp_etl.R $price_data_analysis_input $analysis_output_pdf > $analysis_log"
-echo ""
 # Rscript bootcamp_etl.R $price_data_analysis_input $analysis_output_pdf > $analysis_log
-
+echo ""
 
 
 
---
+++ week_5_code/ex_etl.md
@@ -0,0 +1,19 @@
+
+# Exercise:
+
+1a) Read through master.sh
+
+- There are several new concepts in here, please ask if you see them
+
+
+2) Adjust master.sh to 
+
+
+
+Right now none of the ETL steps are in python, but they all could be.  
+
+1b) Write the python pseudo code you'd use to replace each step in python
+    see week_3_code/stock_api_ans.py
+    see week_3_code/json_io.py
+
+
